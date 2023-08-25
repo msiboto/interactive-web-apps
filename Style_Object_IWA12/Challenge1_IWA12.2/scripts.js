f0ee06a447d4a865c1d4aea3c3685685d9d1a35a
michael <m.siboto88@gmail.com>
2023-08-25 14:02:41 +0200

    commit

--- Style_Object_IWA12/Challenge1_IWA12.2/scripts.js
+++ Style_Object_IWA12/Challenge1_IWA12.2/scripts.js
@@ -36,32 +36,32 @@ const oneCheckout = oneRoot.getElementsByClassName('checkout')[0]
 const oneCheckin = oneRoot.getElementsByClassName('checkin')[0]
 
 const twoRoot = document.getElementById('book2') 
-const twoStatus = twoRoot.getElementsByClassName('status')[1]
-const twoReserve = twoRoot.getElementsByClassName('reserve')[1] 
-const twoCheckout = twoRoot.getElementsByClassName('checkout')[1]
-const twoCheckin = twoRoot.getElementsByClassName('checkin')[1]
+const twoStatus = twoRoot.getElementsByClassName('status')[0]
+const twoReserve = twoRoot.getElementsByClassName('reserve')[0] 
+const twoCheckout = twoRoot.getElementsByClassName('checkout')[0]
+const twoCheckin = twoRoot.getElementsByClassName('checkin')[0]
 
 const threeRoot = document.getElementById('book3') 
-const threeStatus = threeRoot.getElementsByClassName('status')[2]
-const threeReserve = threeRoot.getElementsByClassName('reserve')[2] 
-const threeCheckout = threeRoot.getElementsByClassName('checkout')[2]
-const threeCheckin = threeRoot.getElementsByClassName('checkin')[2]
+const threeStatus = threeRoot.getElementsByClassName('status')[0]
+const threeReserve = threeRoot.getElementsByClassName('reserve')[0] 
+const threeCheckout = threeRoot.getElementsByClassName('checkout')[0]
+const threeCheckin = threeRoot.getElementsByClassName('checkin')[0]
 
-
+ 
 oneCheckin.style.color = ''
-oneStatus.style.color = STATUS_MAP.status.color
-oneReserve = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-oneCheckout = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-oneCheckin = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+oneStatus.style.color = STATUS_MAP.overdue.color 
+oneReserve.disabled = !STATUS_MAP.overdue.canReserve
+oneCheckout.disabled = !STATUS_MAP.overdue.canCheckout 
+oneCheckin.enabled = STATUS_MAP.overdue.canCheckIn
 
 twoCheckin.style.color = ''
-twoStatus.style.color = STATUS_MAP.status.color
-twoReserve = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-twoCheckout = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-twoCheckin = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+twoStatus.style.color = STATUS_MAP.reserved.color
+twoReserve.disabled = !STATUS_MAP.reserved.canReserve
+twoCheckout.enabled = STATUS_MAP.reserved.canCheckout 
+twoCheckin.disabled = !STATUS_MAP.reserved.canCheckIn 
 
 threeCheckin.style.color = ''
-threeStatus.style.color = STATUS_MAP.status.color
-threeReserve = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-threeCheckout = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-threeCheckin = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+threeStatus.style.color = STATUS_MAP.shelf.color
+threeReserve.enabled = STATUS_MAP.shelf.canReserve 
+threeCheckout.enabled= STATUS_MAP.shelf.canCheckout 
+threeCheckin.disabled = !STATUS_MAP.shelf.canCheckIn 
